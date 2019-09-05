e12af5f08dd3de6fac20ec3f0175ac615f293be9
Marknic <marknic@live.com>
2019-09-05 16:09:12 -0500

    adding src

---
+++ src/worker/inst-step01.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+
+# Install Docker - Version 18.09 (this is the latest validated version as of 9/5/19
+wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/containerd.io_1.2.6-3_armhf.deb
+wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/docker-ce-cli_18.09.7~3-0~debian-buster_armhf.deb
+wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/docker-ce_18.09.7~3-0~debian-buster_armhf.deb
+
+sudo dpkg -i containerd.io_1.2.6-3_armhf.deb
+sudo dpkg -i docker-ce-cli_18.09.7~3-0~debian-buster_armhf.deb
+sudo dpkg -i docker-ce_18.09.7~3-0~debian-buster_armhf.deb
+
+sudo usermod pi -aG docker
+
+echo "Logout and back in and then run inst-step02.sh"
+
---
+++ src/worker/inst-step03.sh
@@ -0,0 +1,5 @@
+
+
+
+kubeadm join 192.168.2.101:6443 --token g3wux2.oc3vwunaXXXXXXXX \
+    --discovery-token-ca-cert-hash sha256:XXXXXXXX237ca288c020b8c0XXXXXXXXdace23744e91028cd0bf1d5fXXXXXXXX
