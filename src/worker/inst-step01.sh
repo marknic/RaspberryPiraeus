a33127a13af29c01301b88b3b136122219e8001f
Mark Nichols <nichols_ms@hotmail.com>
2019-09-06 09:48:41 -0500

    Update inst-step01.sh
    removed update/upgrade

--- src/worker/inst-step01.sh
+++ src/worker/inst-step01.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get -y upgrade
-
-
 # Install Docker - Version 18.09 (this is the latest validated version as of 9/5/19
 wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/containerd.io_1.2.6-3_armhf.deb
 wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/docker-ce-cli_18.09.7~3-0~debian-buster_armhf.deb
