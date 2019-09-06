03864757154baf2eea64594d231c9bd8dc7bf418
Mark Nichols <nichols_ms@hotmail.com>
2019-09-06 10:46:16 -0500

    Update inst-step02.sh
    added the command to execute the script

--- src/worker/inst-step02.sh
+++ src/worker/inst-step02.sh
@@ -2,6 +2,9 @@
 
 # This installs the base instructions up to the point of joining / creating a cluster
 
+# To run this command:
+#  sudo curl -sSL https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/worker/inst-step02.sh | sh
+
 sudo dphys-swapfile swapoff
 sudo dphys-swapfile uninstall
 sudo update-rc.d dphys-swapfile remove
