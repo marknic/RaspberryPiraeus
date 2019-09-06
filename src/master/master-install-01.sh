1aa86bd6346de24d82fc804d9be3fe9d9f460098
Mark Nichols <nichols_ms@hotmail.com>
2019-09-06 15:39:29 -0500

    Update and rename master-inst-01.sh to master-install-01.sh
    initial creation of the master install script #1

---
+++ src/master/master-install-01.sh
@@ -0,0 +1,18 @@
+ #!/bin/bash
+
+# Run sudo apt-get update and sudo apt-get upgrade prior to running this script
+# To run this command:
+#  sudo curl -sSL https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/worker/master-install-01.sh | sh
+
+# Install Docker - Version 18.09 (this is the latest validated version as of 9/5/19
+wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/containerd.io_1.2.6-3_armhf.deb
+wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/docker-ce-cli_18.09.7~3-0~debian-buster_armhf.deb
+wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/docker-ce_18.09.7~3-0~debian-buster_armhf.deb
+
+sudo dpkg -i containerd.io_1.2.6-3_armhf.deb
+sudo dpkg -i docker-ce-cli_18.09.7~3-0~debian-buster_armhf.deb
+sudo dpkg -i docker-ce_18.09.7~3-0~debian-buster_armhf.deb
+
+sudo usermod pi -aG docker
+
+echo "Reboot and then run master-install-02.sh"
