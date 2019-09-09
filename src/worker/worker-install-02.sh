d3e276399815ce3d2e3b51ca0104d55d72037bbc
Mark Nichols <nichols_ms@hotmail.com>
2019-09-09 12:10:23 -0500

    Update worker-install-02.sh
    Added info on steps being taken in the script

--- src/worker/worker-install-02.sh
+++ src/worker/worker-install-02.sh
@@ -11,6 +11,10 @@ sudo dphys-swapfile uninstall
 sudo update-rc.d dphys-swapfile remove
 sudo apt -y purge dphys-swapfile
 
+# Add the GPG key to APT
+# Add the kubernetes package to the apt repository list
+# Update the packages list
+# Install kubeadm
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
   echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
   sudo apt-get update -q && \
