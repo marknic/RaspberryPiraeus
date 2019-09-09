1992ab0692638fefcb8af6cd36c47342bede102a
Marknic <marknic@live.com>
2019-09-09 14:35:33 -0500

    added a hosts file update to the master scripts

---
+++ src/master/update-hosts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script will update the hosts file for network communications.
+#  update the IP addresses, the host names, for each of the k8s nodes
+# make sure you make this call before running the script:
+#  sudo chmod +x update-hosts.sh
+#  Then, call it:
+# sudo ./update-hosts.sh
+
+echo '192.168.8.101  kub-master' >> /etc/hosts
+echo '192.168.8.102  kub-worker-01' >> /etc/hosts
+echo '192.168.8.103  kub-worker-02' >> /etc/hosts
+echo '192.168.8.104  kub-worker-03' >> /etc/hosts
+echo '192.168.8.105  kub-worker-04' >> /etc/hosts
+echo '192.168.8.106  kub-worker-05' >> /etc/hosts
+
