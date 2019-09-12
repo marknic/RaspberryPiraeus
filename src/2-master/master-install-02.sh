48a7c0d24c46900d60d5c2ddcd3d2d1b6a264929
Mark Nichols <nichols_ms@hotmail.com>
2019-09-12 12:04:22 -0500

    Update master-install-02.sh

--- src/2-master/master-install-02.sh
+++ src/2-master/master-install-02.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Run this script (as is):  https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/2-master/master-install-02.sh
+# Run this script (as is):  sudo curl -sSL https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/2-master/master-install-02.sh | sh
 # or
 # Download the script for mods: curl -O https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/2-master/master-install-02.sh
 
