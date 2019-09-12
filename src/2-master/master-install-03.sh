39903ce53f4e1f4ca171e7b0c35068d473816d9d
Mark Nichols <nichols_ms@hotmail.com>
2019-09-12 11:59:56 -0500

    Update master-install-03.sh
    Removed Weave

--- src/2-master/master-install-03.sh
+++ src/2-master/master-install-03.sh
@@ -4,11 +4,5 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-# >>>>>>>>>>>>>>>>
-# This will install Weave for networking within kubernetes - use your own option if you wish
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-# >>>>>>>>>>>>>>>>
-
-
 # Your master node should now be operational
 # Test with: kubectl get nodes
