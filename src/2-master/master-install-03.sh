ed1bb8800dcd5f43b66593b66af9e272628965b4
Mark Nichols <nichols_ms@hotmail.com>
2019-09-12 12:05:21 -0500

    Update master-install-03.sh

--- src/2-master/master-install-03.sh
+++ src/2-master/master-install-03.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Run this script (as is):  sudo curl -sSL https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/2-master/master-install-03.sh | sh
+# or
+# Download the script for mods: curl -O https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/2-master/master-install-03.sh
+
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
