15a27230e7a97354b8e490e64725c7fd3c5159fa
Mark Nichols <nichols_ms@hotmail.com>
2019-09-13 10:40:57 -0500

    Update and rename worker-install-01.sh to 1_worker_install.sh

--- src/3-worker/1_worker_install.sh
+++ src/3-worker/1_worker_install.sh
@@ -2,7 +2,7 @@
 
 # Run sudo apt-get update and sudo apt-get upgrade prior to running this script
 # To run this command:
-#  sudo curl -sSL https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/worker/worker-install-01.sh | sh
+#  sudo curl -sSL https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/worker/1_worker_install.sh | sh
 
 # Install Docker - Version 18.09 (this is the latest validated version as of 9/5/19
 wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/containerd.io_1.2.6-3_armhf.deb
@@ -15,5 +15,5 @@ sudo dpkg -i docker-ce_18.09.7~3-0~debian-buster_armhf.deb
 
 sudo usermod pi -aG docker
 
-echo "Reboot and then run worker-install-02.sh"
+echo "Reboot and then run 2_worker_install.sh"
 
