96c406f0a188bc946494e2327a81558220826c39
Mark Nichols <nichols_ms@hotmail.com>
2019-09-13 10:21:14 -0500

    Update and rename master-install-01.sh to 1_master_install.sh

--- src/2-master/1_master_install.sh
+++ src/2-master/1_master_install.sh
@@ -2,9 +2,9 @@
 
 # Run sudo apt-get update and sudo apt-get upgrade prior to running this script
 # To run this command:
-# Run this script (as is):  sudo curl -sSL https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/2-master/master-install-01.sh | sh
+# Run this script (as is):  sudo curl -sSL https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/2-master/1_master_install.sh | sh
 # or
-# Download the script for mods: curl -O https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/2-master/master-install-01.sh
+# Download the script for mods: curl -O https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/2-master/1_master_install.sh
 
 # Install Docker - Version 18.09 (this is the latest validated version as of 9/5/19
 wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/containerd.io_1.2.6-3_armhf.deb
