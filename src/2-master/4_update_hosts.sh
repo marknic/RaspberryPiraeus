da936ec00f34e6e85fdf88633685689b5b346352
Mark Nichols <nichols_ms@hotmail.com>
2019-09-13 10:26:05 -0500

    Update and rename update-hosts.sh to 4_update_hosts.sh

--- src/2-master/4_update_hosts.sh
+++ src/2-master/4_update_hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Download the script for mods: curl -O https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/2-master/update-hosts.sh
+# Download the script for mods: curl -O https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/2-master/4_update_hosts.sh
 
 # This script will update the hosts file for network communications.
 #  update the IP addresses, the host names, for each of the k8s nodes
