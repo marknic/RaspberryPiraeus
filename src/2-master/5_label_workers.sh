b8ae1a685bef711076ad053fc7be118fa16fad73
Mark Nichols <nichols_ms@hotmail.com>
2019-09-13 10:31:27 -0500

    Update 5_label_workers.sh

--- src/2-master/5_label_workers.sh
+++ src/2-master/5_label_workers.sh
@@ -2,7 +2,7 @@
 
 printf "\n\n    WORKER NODE LABELING\n\n"
 
-FILE="update-hosts.sh"
+FILE="4_update_hosts.sh"
 
 if [ -f $FILE ]; then
    printf "File $FILE exists - continuing.\n\n"
@@ -16,7 +16,7 @@ fi
 
 printf "1. Run this script \e[1;31mONLY AFTER\e[0m all of the worker nodes have been created.\n"
 printf "2. Run this script \e[1;31mONLY ON\e[0m the master node.\n"
-printf "3. Run this script \e[1;31mONLY IF\e[0m the file update-hosts.sh has been updated with the worker node names.\n\n"
+printf "3. Run this script \e[1;31mONLY IF\e[0m the file 4_update_hosts.sh has been updated with the worker node names.\n\n"
 
 while true; do
     read -p "Do you want to run the script (y/n)?" yn
@@ -28,7 +28,7 @@ while true; do
 done
 
 
-ip_addr=$(cat update-hosts.sh | grep -Eo "?([0-9]*\.){3}[0-9]*.*$(hostname).*" | grep -Eo '([0-9]*\.){3}[0-9]*')
+ip_addr=$(cat $FILE | grep -Eo "?([0-9]*\.){3}[0-9]*.*$(hostname).*" | grep -Eo '([0-9]*\.){3}[0-9]*')
 
 host_name="$(hostname)"
 
