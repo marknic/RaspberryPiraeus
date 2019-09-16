5a0090c59fcaf3786354ba12f34e20a5a101b2a4
Mark Nichols <nichols_ms@hotmail.com>
2019-09-16 09:26:28 -0500

    Update automatic_install.sh

--- src/automation/automatic_install.sh
+++ src/automation/automatic_install.sh
@@ -1,1 +1,6 @@
-#/bin/bash
+#!/bin/bash
+
+pword="raspberry"
+id="pi"
+
+sshpass -p raspberry ssh pi@192.168.8.101 "curl -sSL https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/1-node-prep/1_setup_files_locally.sh | sh"
