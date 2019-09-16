bd7373a74ec8078253f6711bb57654b65e0b2b1d
Mark Nichols <nichols_ms@hotmail.com>
2019-09-16 09:38:58 -0500

    Update automatic_install.sh

--- src/automation/automatic_install.sh
+++ src/automation/automatic_install.sh
@@ -4,6 +4,6 @@ pword="raspberry"
 id="pi"
 ip="192.168.8.101"
 
-sshpass -p "$pword" ssh "$id@$ip" "curl -sSL https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/1-node-prep/copy_hostname_scripts.sh.sh | sh"
+sshpass -p "$pword" ssh $id@$ip "curl -sSL https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/1-node-prep/copy_hostname_scripts.sh.sh | sh"
 
 
