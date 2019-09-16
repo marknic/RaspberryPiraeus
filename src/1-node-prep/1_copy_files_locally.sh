8d6bd5ea57a949dd9616d21b85e4c525d4529f4c
Mark Nichols <nichols_ms@hotmail.com>
2019-09-16 08:05:26 -0500

    Update 1_copy_files_locally.sh

--- src/1-node-prep/1_copy_files_locally.sh
+++ src/1-node-prep/1_copy_files_locally.sh
@@ -3,9 +3,17 @@
 # Execute this script with the following command:
 #  curl -sSL https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/1-node-prep/1_copy_files_locally.sh | sh
 
-curl -O https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/1-node-prep/2_copy_hostname_script.sh
+if [ ! -f 2_copy_hostname_script.sh ]; then
+  curl -O https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/1-node-prep/2_copy_hostname_script.sh
+fi
+
+if [ ! -f 3_set_hostname.sh ]; then
 curl -O https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/1-node-prep/3_set_hostname.sh
+fi
+
+if [ ! -f 4_update_hosts.sh ]; then
 curl -O https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/1-node-prep/4_update_hosts.sh
+fi
 
 chmod +x 2_copy_hostname_script.sh
 chmod +x 3_set_hostname.sh
