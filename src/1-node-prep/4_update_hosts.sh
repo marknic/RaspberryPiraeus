afc4dfa879a24504ac75f8f8fd3f294a3ccdb3ec
Mark Nichols <nichols_ms@hotmail.com>
2019-09-16 08:21:40 -0500

    Update 4_update_hosts.sh

--- src/1-node-prep/4_update_hosts.sh
+++ src/1-node-prep/4_update_hosts.sh
@@ -6,14 +6,9 @@
 #   it will make things much easier.  Note: this file exists in 3 folders including master and worker but it is exactly the same 
 #   structure and data - it is also named the same across all folders.  One version is all you need.
 
-# Download the script for mods: curl -O https://raw.githubusercontent.com/marknic/RaspberryPiraeus/master/src/1-node-prep/4_update_hosts.sh
-
 # This script will update the hosts file for network communications.
 #  update the IP addresses, the host names, for each of the k8s nodes
 # make sure you make this call before running the script:
-#  sudo chmod +x 4_update_hosts.sh
-#  Then, call it:
-# sudo ./4_update_hosts.sh
 
 echo '192.168.8.100  kub-master' >> /etc/hosts
 echo '192.168.8.101  kub-worker-01' >> /etc/hosts
