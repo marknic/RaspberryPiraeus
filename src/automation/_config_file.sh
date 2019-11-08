5c65478166ec18ff5de6bcba39e06aa5ad13a36c
Marknic <marknic@live.com>
2019-11-08 15:24:26 -0600

    comment about docker version

--- src/automation/_config_file.sh
+++ src/automation/_config_file.sh
@@ -8,6 +8,7 @@ clusterfile="_cluster.json"
 
 download_location="https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/"
 
+# Docker version 18.09.7~3 is a varified version for use with Kubernetes
 containerd="containerd.io_1.2.6-3_armhf.deb"
 docker_ce_cli="docker-ce-cli_18.09.7~3-0~debian-buster_armhf.deb"
 docker_ce="docker-ce_18.09.7~3-0~debian-buster_armhf.deb"
