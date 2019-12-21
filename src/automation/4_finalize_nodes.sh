56cb8467ec8dc2d745c318d373637a9d16c1e70c
Mark Nichols <nichols_ms@hotmail.com>
2019-12-21 16:56:45 -0600

    Removed some networking cmds
    Modified script because of different version of Flannel - v0.9.1

--- src/automation/4_finalize_nodes.sh
+++ src/automation/4_finalize_nodes.sh
@@ -15,24 +15,27 @@ joincmd=$(sudo kubeadm token create --print-join-command)
 # Load Flannel for networking - Note: Change this command if you don't want to use Flannel
 printf "\nInstalling Flannel\n\n"
 #kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/a70459be0084506e4ec919aa1c114638878db11b/Documentation/kube-flannel.yml
-sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
-sudo kubectl taint nodes $(hostname) node-role.kubernetes.io/master=true:NoSchedule
-sudo kubectl label node $(hostname) kubernetes.io/role=master node-role.kubernetes.io/master=
+# One of these...
+#sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+curl -sSL https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml | sed "s/amd64/arm64/g" | kubectl create -f -
 
-sudo iptables -P FORWARD ACCEPT
-sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+#sudo kubectl taint nodes $(hostname) node-role.kubernetes.io/master=true:NoSchedule
+#sudo kubectl label node $(hostname) kubernetes.io/role=master node-role.kubernetes.io/master=
+
+#sudo iptables -P FORWARD ACCEPT
+#sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
 
 
 # Delete network interface(s) that match 'master cni0'
-ip link show 2>/dev/null | grep 'master cni0' | while read ignore iface ignore; do
-    iface=${iface%%@*}
-    [ -z "$iface" ] || ip link delete $iface
-done
-ip link delete cni0
-ip link delete flannel.1
-rm -rf /var/lib/cni/
-iptables-save | grep -v KUBE- | grep -v CNI- | iptables-restore
+#ip link show 2>/dev/null | grep 'master cni0' | while read ignore iface ignore; do
+#    iface=${iface%%@*}
+#    [ -z "$iface" ] || ip link delete $iface
+#done
+#ip link delete cni0
+#ip link delete flannel.1
+#rm -rf /var/lib/cni/
+#iptables-save | grep -v KUBE- | grep -v CNI- | iptables-restore
 
 
 for ((i=0; i<$length; i++));
