fabe43374d2cb2b07d1272baea1f23ad739a24ef
Mark Nichols <nichols_ms@hotmail.com>
2019-12-23 16:44:11 -0600

    aligned master and worker commands

--- src/automation/3.1_install_kubernetes.sh
+++ src/automation/3.1_install_kubernetes.sh
@@ -23,15 +23,18 @@ sudo dphys-swapfile uninstall
 sudo update-rc.d dphys-swapfile remove
 sudo apt-get -y purge dphys-swapfile
 
+print_instruction "\nUpdate and Upgrade"
 sudo apt update
-sudo apt upgrade -y
+sudo apt -y upgrade
 
+print_instruction "\nModify IPTables"
 sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
 sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 sudo update-alternatives --set ebtables /usr/sbin/ebtables-legacy
 
-
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+print_instruction "\nUpdate and install"
+sudo apt-get update
+sudo apt-get install -y apt-transport-https curl
 
 
 print_instruction "\nAdding link to Kubernetes repository and adding the APT key\n"
@@ -39,7 +42,6 @@ sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-ke
 
 # Create a support file that will be copied to the nodes
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee kubernetes.list
-
 
 # Add Kubernetes repository to the RPi package lists
 sudo rm -f /etc/apt/sources.list.d/kubernetes.list
@@ -49,7 +51,6 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 
 print_instruction "Configuring Kubernetes with local user"
-
 sudo -u $piid ./_kube_config.sh
 
 sudo apt-mark hold kubelet kubeadm kubectl
@@ -57,6 +58,9 @@ sudo apt-mark hold kubelet kubeadm kubectl
 print_instruction "\nkubeadm init...\n"
 sudo kubeadm init --ignore-preflight-errors=all --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address=$ip_addr_me
 
+ # Do some cleanup
+print_instruction "\nDo some cleanup: autoremove\n"
+sudo sshpass -p $pword ssh $piid@$ip_target sudo apt -y autoremove
 
 
 
@@ -68,52 +72,79 @@ do
     ip_target=$(echo $cluster_data | jq --raw-output ".[$i].IP")
     host_target=$(echo $cluster_data | jq --raw-output ".[$i].name")
 
-    print_instruction "\n\n-----------\n"
+    print_instruction "\n-----------"
     print_instruction "Configuring $host_target/$ip_target\n"
 
     if [ $ip_target != $ip_addr_me ]
     then
 
         # Run this code across all machines
-        print_instruction "Removing swapfile on $host_target.\n"
+        print_instruction "\nRemoving swapfile on $host_target."
         sudo sshpass -p $pword ssh $piid@$ip_target sudo dphys-swapfile swapoff
         sudo sshpass -p $pword ssh $piid@$ip_target sudo dphys-swapfile uninstall
         sudo sshpass -p $pword ssh $piid@$ip_target sudo update-rc.d dphys-swapfile remove
         sudo sshpass -p $pword ssh $piid@$ip_target sudo apt-get -y purge dphys-swapfile
 
-        print_instruction "Copying kubernetes.list to worker machine.\n"
-        sudo sshpass -p $pword sudo scp "kubernetes.list"  $piid@$ip_target:
+        print_instruction "\nUpdate and install"
+        sudo sshpass -p $pword ssh $piid@$ip_target sudo apt-get update
+        sudo sshpass -p $pword ssh $piid@$ip_target sudo apt-get install -y apt-transport-https curl
 
         print_instruction "\nAdding link to Kubernetes repository and adding the APT key\n"
+        sudo sshpass -p $pword ssh $piid@$ip_target sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+        # Create a support file that will be copied to the nodes
+        sudo sshpass -p $pword ssh $piid@$ip_target echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee kubernetes.list
+
         # Add Kubernetes repository to the RPi package lists
-        sudo sshpass -p $pword ssh $piid@$ip_target "sudo rm -f /etc/apt/sources.list.d/kubernetes.list"
-        sudo sshpass -p $pword ssh $piid@$ip_target "sudo mv -f kubernetes.list /etc/apt/sources.list.d/kubernetes.list"
-        sudo sshpass -p $pword ssh $piid@$ip_target sudo apt-key adv --fetch-keys https://packages.cloud.google.com/apt/doc/apt-key.gpg
+        sudo sshpass -p $pword ssh $piid@$ip_target sudo rm -f /etc/apt/sources.list.d/kubernetes.list
+        sudo sshpass -p $pword ssh $piid@$ip_target sudo cp -f kubernetes.list /etc/apt/sources.list.d/kubernetes.list
 
-        sudo sshpass -p $pword ssh $piid@$ip_target sudo apt -y update && sudo apt -y upgrade
+        sudo sshpass -p $pword ssh $piid@$ip_target sudo apt-get update
+        sudo sshpass -p $pword ssh $piid@$ip_target sudo apt-get install -y kubelet kubeadm kubectl
 
-        print_instruction "Backing up /boot/cmdline.txt\n"
-        sudo sshpass -p $pword ssh $piid@$ip_target sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
+        print_instruction "Configuring Kubernetes with local user"
+        sudo sshpass -p $pword ssh $piid@$ip_target sudo -u $piid ./_kube_config.sh
 
-        sudo sshpass -p $pword ssh $piid@$ip_target echo "$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" | sudo tee /boot/cmdline.txt
-
-
-
-        print_instruction "\napt-get -y update\n"
-        sudo sshpass -p $pword ssh $piid@$ip_target sudo apt-get -y update
-
-        print_instruction "\napt-get -y install dnsutils kubeadm\n"
-        sudo sshpass -p $pword ssh $piid@$ip_target sudo apt-get -y install dnsutils kubeadm
+        print_instruction "Locking: kubelet kubeadm kubectl"
+        sudo sshpass -p $pword ssh $piid@$ip_target sudo apt-mark hold kubelet kubeadm kubectl
 
         # Do some cleanup
-        print_instruction "\napt -y autoremove\n"
+        print_instruction "\nDo some cleanup: autoremove\n"
         sudo sshpass -p $pword ssh $piid@$ip_target sudo apt -y autoremove
 
+
+        # print_instruction "\nCopying kubernetes.list to worker machine."
+        # sudo sshpass -p $pword sudo scp "kubernetes.list" $piid@$ip_target:
+
+        # print_instruction "\nAdding link to Kubernetes repository and adding the APT key."
+        # # Add Kubernetes repository to the RPi package lists
+        # sudo sshpass -p $pword ssh $piid@$ip_target sudo rm -f /etc/apt/sources.list.d/kubernetes.list
+        # sudo sshpass -p $pword ssh $piid@$ip_target sudo mv -f kubernetes.list /etc/apt/sources.list.d/kubernetes.list
+        # sudo sshpass -p $pword ssh $piid@$ip_target sudo apt-key adv --fetch-keys https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+        # print_instruction "\nUpdate and Upgrade"
+        # sudo sshpass -p $pword ssh $piid@$ip_target sudo apt update
+        # sudo sshpass -p $pword ssh $piid@$ip_target sudo apt -y upgrade
+
+
+        # print_instruction "Backing up /boot/cmdline.txt\n"
+        # sudo sshpass -p $pword ssh $piid@$ip_target sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
+
+        # sudo sshpass -p $pword ssh $piid@$ip_target echo "$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" | sudo tee /boot/cmdline.txt
+
+        # print_instruction "\napt-get -y update\n"
+        # sudo sshpass -p $pword ssh $piid@$ip_target sudo apt-get -y update
+
+        # print_instruction "\napt-get -y install dnsutils kubeadm\n"
+        # sudo sshpass -p $pword ssh $piid@$ip_target sudo apt-get -y install dnsutils kubeadm
+
+        # # Do some cleanup
+        # print_instruction "\napt -y autoremove\n"
+        # sudo sshpass -p $pword ssh $piid@$ip_target sudo apt -y autoremove
 
 
         # print_instruction "\nkubeadm init...\n"
         # sudo sshpass -p $pword ssh $piid@$ip_target sudo kubeadm init --ignore-preflight-errors=all --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address=$ip_addr_me
-
 
 
     fi
