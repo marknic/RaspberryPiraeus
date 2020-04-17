fc0e5b2bed0a4211012893d4737193ef3a67d643
Mark Nichols <nichols_ms@hotmail.com>
2020-04-17 10:54:47 -0500

    fixed the setup process - streamlined it

--- src/automation/1_setup_host_ssh.sh
+++ src/automation/1_setup_host_ssh.sh
@@ -2,13 +2,17 @@
 
 . _config_file.sh
 
-print_instruction " _______ _______ _______ _     _  _____       _     _  _____  _______ _______"
-print_instruction " |______ |______    |    |     | |_____]      |_____| |     | |______    |"
-print_instruction " ______| |______    |    |_____| |            |     | |_____| ______|    |\n"
-
-print_instruction " _______ _______ _     _"
-print_instruction " |______ |______ |_____|"
-print_instruction " ______| ______| |     |\n"
+print_instruction "  _____         _             "
+print_instruction " |_   _|__  ___| |_           "
+print_instruction "   | |/ _ \/ __| __|          "
+print_instruction "   | |  __/\__ \ |_           "
+print_instruction "   |_|\___||___/\__|          "
+print_instruction "  ____            _       _   "
+print_instruction " / ___|  ___ _ __(_)_ __ | |_ "
+print_instruction " \___ \ / __| '__| | '_ \| __|"
+print_instruction "  ___) | (__| |  | | |_) | |_ "
+print_instruction " |____/ \___|_|  |_| .__/ \__|"
+print_instruction "                   |_|       \n"
 
 . _check_root.sh
 
@@ -18,14 +22,11 @@ print_instruction " ______| ______| |     |\n"
 
 printf "Setting up SSH.\n\n"
 
-# Set up SSH keys
+# # Set up SSH keys
 ssh-keygen -t rsa -b 2048 -f /home/$piid/.ssh/id_rsa -N ""
 sudo chown -R $piid /home/$piid/.ssh/
 
 printf "Done creating SSH Keys.\n\n"
-
-printf "${CYAN}>> Setting up host names and IP's.${NC}\n\n"
-# Clean up the hosts file before attempting to update with current information
 
 for ((i=0; i<$length; i++));
 do
@@ -33,111 +34,64 @@ do
     ip_target=$(echo $cluster_data | jq --raw-output ".[$i].IP")
     new_host_name=$(echo $cluster_data | jq --raw-output ".[$i].name")
 
-    if [ $ip_target != $ip_addr_me ]; then
-        sudo sshpass -p $pword ssh -o "StrictHostKeyChecking=no" $piid@$ip_target sudo mkdir /home/pi/.ssh/
-        sudo sshpass -p $pword ssh $piid@$ip_target sudo chown pi /home/pi/.ssh/
-        sudo sshpass -p $pword scp -p -r /home/pi/.ssh/id_rsa.pub $piid@$ip_target:/home/pi/.ssh/authorized_keys
-    fi
-
-    printf "\n${CYAN}>> Updating the hosts and hostname files on $ip_target.${NC}\n\n"
-    # Delete the local host files (quietly - they may not exist)
-    sudo rm -f $localhostsfile > /dev/null 2>&1
-    sudo rm -f $localhostnamefile > /dev/null 2>&1
-
-    printf "${CYAN}>> copy /etc/hosts${NC}\n"
-
-    if [ $ip_target = $ip_addr_me ]
-    then
-        sudo cp $FILE_HOSTS $localhostsfile
-        printf "done: cp $FILE_HOSTS\n"
-    else
-        # Copy machine host file to local host file
-        #sudo sshpass -p $pword scp "$piid@$ip_target:$FILE_HOSTS" $localhostsfile
-        scp "$piid@$ip_target:$FILE_HOSTS" $localhostsfile
-    fi
-
-    if ! test -f $localhostsfile; then
-        cp -f _hosts.data $localhostsfile
-    fi
-
-    sudo chown $piid $localhostsfile
-    sed -i -e "/127.0.1.1/d" $localhostsfile
-
-    echo "127.0.1.1    $new_host_name" >> $localhostsfile
+    rm -f $localhostsfile > /dev/null 2>&1
+    rm -f $localhostnamefile > /dev/null 2>&1
 
     # Create the hostname file (to be copied to the remote machine's /etc/ folder)
     echo "$new_host_name" > $localhostnamefile
 
-    j=0
-    while [ $j -lt $length ]
+    if [ $ip_target == $ip_addr_me ]; then
+        cp $FILE_HOSTS $localhostsfile
+    else
+        sudo sshpass -p $pword ssh -o "StrictHostKeyChecking=no" $piid@$ip_target sudo mkdir /home/$piid/.ssh/
+        sudo sshpass -p $pword ssh $piid@$ip_target sudo chown -R $piid /home/$piid/.ssh/
+        sudo sshpass -p $pword scp -p -r /home/$piid/.ssh/id_rsa.pub $piid@$ip_target:/home/$piid/.ssh/authorized_keys
+
+        sshpass -p $pword scp "$piid@$ip_target:$FILE_HOSTS" $localhostsfile
+    fi
+
+    # Remove the line with 127.0.1.1 in it
+    sed -i -e "/127.0.1.1/d" $localhostsfile
+
+    # Replace the removed line with the updated line
+    echo "127.0.1.1       $new_host_name" >> $localhostsfile
+
+    print_instruction "Adding other cluster hosts to the current hosts file."
+    for ((j=0; j<$length; j++));
     do
         ip_to_change=$(echo $cluster_data | jq --raw-output ".[$j].IP")
-
-        # Indicate that work is being done
-        printf "."
-
-        # Delete the lines containing the IP address
-        sed -i -e "/$ip_to_change/d" $localhostsfile
 
         if [ $ip_target != $ip_to_change ]; then
 
             host_to_add=$(echo $cluster_data | jq --raw-output ".[$j].name")
 
-            echo "$ip_to_change  $host_to_add" >> $localhostsfile
-
-            # Indicate that work is being done
-            printf "-"
+            echo "$ip_to_change   $host_to_add" >> $localhostsfile
         fi
-
-        ((j++))
     done
 
-    # Remove the redundant entry in the hosts file
-    sed -i -e "/$ip_target/d" $localhostsfile
-
-    printf "."
-
-
-    if [ $ip_target = $ip_addr_me ]
-    then
-        # Replace the machine hosts/hostname files
+    if [ $ip_target == $ip_addr_me ]; then
         sudo rm -f $FILE_HOSTS
         sudo rm -f $FILE_HOSTNAME
 
-        printf "."
-
-        sudo mv -f $localhostsfile  $FILE_HOSTS
+        sudo mv -f $localhostsfile     $FILE_HOSTS
         sudo mv -f $localhostnamefile  $FILE_HOSTNAME
-
-        printf "."
     else
-        # Replace the machine hosts/hostname files
+        print_instruction "Copy the host files over to the worker(s)."
+        sshpass -p $pword scp $localhostnamefile  $piid@$ip_target:
+        sshpass -p $pword scp $localhostsfile     $piid@$ip_target:
 
-        #sudo sshpass -p $pword sudo scp $localhostnamefile  $piid@$ip_target:
-        #sudo sshpass -p $pword sudo scp $localhostsfile  $piid@$ip_target:
-        sudo scp $localhostnamefile  $piid@$ip_target:
-        sudo scp $localhostsfile  $piid@$ip_target:
+        print_instruction "Remove the existing host files."
+        sshpass -p $pword ssh $piid@$ip_target sudo rm -f $FILE_HOSTS
+        sshpass -p $pword ssh $piid@$ip_target sudo rm -f $FILE_HOSTNAME
 
-        printf "."
-
-        #sshpass -p $pword ssh $piid@$ip_target "sudo rm -f $FILE_HOSTS"
-        #sshpass -p $pword ssh $piid@$ip_target "sudo mv -f $localhostsfile $FILE_HOSTS"
-        ssh $piid@$ip_target "sudo rm -f $FILE_HOSTS"
-        ssh $piid@$ip_target "sudo mv -f $localhostsfile $FILE_HOSTS"
-
-        printf "."
-
-        #sshpass -p $pword ssh $piid@$ip_target "sudo rm -f $FILE_HOSTNAME"
-        #sshpass -p $pword ssh $piid@$ip_target "sudo mv -f $localhostnamefile $FILE_HOSTNAME"
-        ssh $piid@$ip_target "sudo rm -f $FILE_HOSTNAME"
-        ssh $piid@$ip_target "sudo mv -f $localhostnamefile $FILE_HOSTNAME"
-
-        printf "."
+        print_instruction "Move the new host files into /etc/.\n"
+        sshpass -p $pword ssh $piid@$ip_target sudo mv -f $localhostsfile    $FILE_HOSTS
+        sshpass -p $pword ssh $piid@$ip_target sudo mv -f $localhostnamefile $FILE_HOSTNAME
     fi
-
-    printf "!\n\n"
 
 done
 
-. _worker_reboot.sh
 
+print_instruction "\nDone!\n"
+
+. _worker_reboot.sh
--- src/automation/test-it.sh
+++ src/automation/test-it.sh
@@ -22,7 +22,7 @@ print_instruction "                   |_|       \n"
 
 printf "Setting up SSH.\n\n"
 
-# Set up SSH keys
+# # Set up SSH keys
 ssh-keygen -t rsa -b 2048 -f /home/$piid/.ssh/id_rsa -N ""
 sudo chown -R $piid /home/$piid/.ssh/
 
@@ -34,13 +34,64 @@ do
     ip_target=$(echo $cluster_data | jq --raw-output ".[$i].IP")
     new_host_name=$(echo $cluster_data | jq --raw-output ".[$i].name")
 
-    if [ $ip_target != $ip_addr_me ]; then
+    rm -f $localhostsfile > /dev/null 2>&1
+    rm -f $localhostnamefile > /dev/null 2>&1
+
+    # Create the hostname file (to be copied to the remote machine's /etc/ folder)
+    echo "$new_host_name" > $localhostnamefile
+
+    if [ $ip_target == $ip_addr_me ]; then
+        cp $FILE_HOSTS $localhostsfile
+    else
         sudo sshpass -p $pword ssh -o "StrictHostKeyChecking=no" $piid@$ip_target sudo mkdir /home/$piid/.ssh/
         sudo sshpass -p $pword ssh $piid@$ip_target sudo chown -R $piid /home/$piid/.ssh/
         sudo sshpass -p $pword scp -p -r /home/$piid/.ssh/id_rsa.pub $piid@$ip_target:/home/$piid/.ssh/authorized_keys
+
+        sshpass -p $pword scp "$piid@$ip_target:$FILE_HOSTS" $localhostsfile
     fi
+
+    # Remove the line with 127.0.1.1 in it
+    sed -i -e "/127.0.1.1/d" $localhostsfile
+
+    # Replace the removed line with the updated line
+    echo "127.0.1.1       $new_host_name" >> $localhostsfile
+
+    print_instruction "Adding other cluster hosts to the current hosts file."
+    for ((j=0; j<$length; j++));
+    do
+        ip_to_change=$(echo $cluster_data | jq --raw-output ".[$j].IP")
+
+        if [ $ip_target != $ip_to_change ]; then
+
+            host_to_add=$(echo $cluster_data | jq --raw-output ".[$j].name")
+
+            echo "$ip_to_change   $host_to_add" >> $localhostsfile
+        fi
+    done
+
+    if [ $ip_target == $ip_addr_me ]; then
+        sudo rm -f $FILE_HOSTS
+        sudo rm -f $FILE_HOSTNAME
+
+        sudo mv -f $localhostsfile     $FILE_HOSTS
+        sudo mv -f $localhostnamefile  $FILE_HOSTNAME
+    else
+        print_instruction "Copy the host files over to the worker(s)."
+        sshpass -p $pword scp $localhostnamefile  $piid@$ip_target:
+        sshpass -p $pword scp $localhostsfile     $piid@$ip_target:
+
+        print_instruction "Remove the existing host files."
+        sshpass -p $pword ssh $piid@$ip_target sudo rm -f $FILE_HOSTS
+        sshpass -p $pword ssh $piid@$ip_target sudo rm -f $FILE_HOSTNAME
+
+        print_instruction "Move the new host files into /etc/.\n"
+        sshpass -p $pword ssh $piid@$ip_target sudo mv -f $localhostsfile    $FILE_HOSTS
+        sshpass -p $pword ssh $piid@$ip_target sudo mv -f $localhostnamefile $FILE_HOSTNAME
+    fi
+
 done
+
 
 print_instruction "\nDone!\n"
 
-#. _worker_reboot.sh
+. _worker_reboot.sh
