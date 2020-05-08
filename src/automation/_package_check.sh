f0cdaccd36f5868514a083481621affeb451e72e
Mark Nichols <nichols_ms@hotmail.com>
2020-05-08 15:24:17 -0500

    Added a function to install and validate linux packages

--- src/automation/_package_check.sh
+++ src/automation/_package_check.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-printf "\n Verifying Package:\n    jq status: "
+install_and_validate_package() {
+    local -r -i max_attempts=5
+
+    local -i attempt_num=1
+
+    until dpkg-query -W -f='${Status}\n' $1 > /dev/null 2>&1
+    do
+        if (( attempt_num == max_attempts ))
+        then
+            echo "Attempt $attempt_num failed and there are no more attempts left!"
+            return 1
+        else
+            echo "Attempting to install package: $1  This is attempt $attempt_num."
+            (( attempt_num++ ))
+            sudo apt-get install -y jq
+        fi
+    done
+}
+
+printf "\n Verifying Package:\n    jq "
 # install jq here - it is used to parse the json data
-dpkg-query -W -f='${Status}\n' jq | grep 'ok installed'
+install_and_validate_package jq
 
-result=$?
 
-printf "\n"
-
-if [ $result = 1 ]
-then
-    echo "Installing package: jq"
-    sudo apt-get install -y jq
-fi
-
-printf "\n Verifying Package:\n    sshpass status: "
+printf "\n Verifying Package:\n    sshpass"
 # install sshpass here - it is used to parse the json data
-dpkg-query -W -f='${Status}\n' sshpass | grep 'ok installed'
-
-result=$?
-
-printf "\n"
-
-if [ $result = 1 ]
-then
-    echo "Installing package: sshpass"
-    sudo apt-get install -y sshpass
-fi
+install_and_validate_package sshpass
 
