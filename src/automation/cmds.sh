f9372121004ef125cb3f8f8a568b8a74b69b9a8f
Mark Nichols <nichols_ms@hotmail.com>
2020-06-02 14:33:25 -0500

    Title in the cmds.sh file

--- src/automation/cmds.sh
+++ src/automation/cmds.sh
@@ -2,6 +2,13 @@
 
 . _config_file.sh
 
+print_instruction "   ____                                          _         "
+print_instruction "  / ___|___  _ __ ___  _ __ ___   __ _ _ __   __| |___     "
+print_instruction " | |   / _ \| !_ ! _ \| !_ ! _ \ / _! | !_ \ / _! / __|    "
+print_instruction " | |__| (_) | | | | | | | | | | | (_| | | | | (_| \__ \    "
+print_instruction "  \____\___/|_| |_| |_|_| |_| |_|\__,_|_| |_|\__,_|___/  \n"
+
+
 print_instruction "\nExecute the scripts in this way:"
 print_instruction "sudo ./1_setup_ssh.sh"
 print_instruction "sudo ./1.1_locale_and_time.sh"
