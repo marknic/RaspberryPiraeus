2470341130150efa6b2990871f1f063ab0e10549
Mark Nichols <nichols_ms@hotmail.com>
2020-06-02 14:26:42 -0500

    cmds.sh

--- src/automation/cmds.sh
+++ src/automation/cmds.sh
@@ -8,5 +8,5 @@ print_instruction "sudo ./1.1_locale_and_time.sh"
 print_instruction "sudo ./2_swap_file.sh"
 print_instruction "sudo ./3_install_docker.sh"
 print_instruction "sudo ./4_kube_install.sh"
-print_instruction "./cmds.sh"
+print_instruction "./cmds.sh\n\n"
 
